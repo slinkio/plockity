b645d69e07b7499a2c91ced6ab966190fe3b2dfd
James-Mac <james@aehr.org>
2014-07-24 20:11:09 -0600

    Styling

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: [ 'application' ],
+
+  routes: {
+    'account.index': {
+      description: "Overview"
+    },
+    'account.apps': {
+      description: "Manage Applications"
+    }
+  },
+
   subroute: function () {
-    return this.get('currentPath');
-  }.property('currentPath')
+    var routes = this.get('routes'),
+        route  = routes[this.get('controllers.application.currentPath')];
+    return (route) ? route.description : null;
+  }.property('controllers.application.currentPath')
 });
